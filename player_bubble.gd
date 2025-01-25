fd8da510379b403254e0de26906da0c8051e5875
nmuranovas <nerijus.muranovas@gmail.com>
2025-01-25 15:16:23 +0200

    Add player_bubble

---
+++ player_bubble.gd
@@ -0,0 +1,28 @@
+extends RigidBody3D
+
+@onready var camera: Camera3D = $Camera
+@export var move_speed : float = 25.0
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_forward"):
+		apply_central_force(-camera.global_transform.basis.z * move_speed )
+	if Input.is_action_pressed("move_back"):
+		apply_central_force(camera.global_transform.basis.z * move_speed)
+	if Input.is_action_pressed("move_left"):
+		apply_central_force(-camera.global_transform.basis.x * move_speed)
+	if Input.is_action_pressed("move_right"):
+		apply_central_force(camera.global_transform.basis.x * move_speed)
+	if Input.is_action_pressed("move_up"):
+		apply_central_force(camera.global_transform.basis.y * move_speed)
+	if Input.is_action_pressed("move_down"):
+		apply_central_force(-camera.global_transform.basis.y * move_speed)
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate(Vector3.UP, -event.relative.x * 0.001)
+		rotate_object_local(Vector3.RIGHT, -event.relative.y * 0.001)
+		print(camera.global_transform.basis.z)
